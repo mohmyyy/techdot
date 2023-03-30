a4d0d99e0a943165740e59c1d1940d9621efcb5a
mohmyyy <iammohmy@gmail.com>
2023-03-31 02:56:25 +0530

    Login Screen TDD

--- src/Pages/login-signup.test.js
+++ src/Pages/login-signup.test.js
@@ -1,20 +1,82 @@
 import { render, screen } from "@testing-library/react";
 import Register from "./Register";
+import Login from "./Login";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 describe("signup UI", () => {
   test("email input", () => {
-    render(<Register />);
-    const nameElement = screen.getByText('Email')
-    expect(nameElement).toBeInTheDocument()
+    render(
+      <Provider store={store}>
+        <Register />
+      </Provider>
+    );
+    const nameElement = screen.getByText("Email");
+    expect(nameElement).toBeInTheDocument();
   });
   test("password input", () => {
-    render(<Register />);
-    const passwordElement = screen.getByText('Password')
-    expect(passwordElement).toBeInTheDocument()
+    render(
+      <Provider store={store}>
+        <Register />
+      </Provider>
+    );
+    const passwordElement = screen.getByText("Password");
+    expect(passwordElement).toBeInTheDocument();
   });
   test("confirm password input", () => {
-    render(<Register />);
-    const confirmPasswordElement = screen.getByText('Confirm Password')
-    expect(confirmPasswordElement).toBeInTheDocument()
+    render(
+      <Provider store={store}>
+        <Register />
+      </Provider>
+    );
+    const confirmPasswordElement = screen.getByText("Confirm Password");
+    expect(confirmPasswordElement).toBeInTheDocument();
+  });
+});
+describe("Login UI", () => {
+  test("email input", () => {
+    render(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+    const nameElement = screen.getByText("Email");
+    expect(nameElement).toBeInTheDocument();
+  });
+  test("password input", () => {
+    render(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+    const passwordElement = screen.getByText("Password");
+    expect(passwordElement).toBeInTheDocument();
+  });
+  test("logIn button", () => {
+    render(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+  test("register anchor", () => {
+    render(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+    const anchor = screen.getByRole("link");
+    expect(anchor).toBeInTheDocument();
+  });
+  test("LogIn header", () => {
+    render(
+      <Provider store={store}>
+        <Login />
+      </Provider>
+    );
+    const header = screen.getByText("Log In");
+    expect(header).toBeInTheDocument();
   });
 });
